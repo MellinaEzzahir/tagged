6348df07b657de0a8f2a170b4d6387e17d5aee53
MellinaEzzahir <mellina.ezzahir@gmail.com>
2025-09-04 20:53:05 +0100

    committed

---
+++ Tagged/lib/app-write.js
@@ -0,0 +1,9 @@
+import { Client, Account, ID, Databases } from 'appwrite';
+
+const client = new Client()
+    .setEndpoint('https://fra.coud.appwrite.io/v1')
+    .setProject('tagged')
+
+const database = new Databases(client);
+const account = new Account(client);
+export {ID, client, database, account};
